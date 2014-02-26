8c4175a2bbdbf97652dabe64c6cb10d43e10f56a
antsao <antsao@235x17.csc.calpoly.edu>
2014-02-25 17:40:48 -0800

    Calculated Mean

--- vec_analytics.cu
+++ vec_analytics.cu
@@ -3,13 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <cmath>
+#include <thrust/host_vector.h>
+#include <thrust/functional.h>
 #include <thrust/device_vector.h>
+#include <thrust/transform_reduce.h>
 #include <unistd.h>
 
 #define FLOAT_SIZE 4
 
 using namespace thrust;
-using namespace std;
 
 void readElements(int fp, int numElements, host_vector<float> *elements) {
    float temp;
@@ -22,6 +25,7 @@ void readElements(int fp, int numElements, host_vector<float> *elements) {
 int main(int argc, char **argv) {
    int fp;
    int numElements;
+   float mean;
 
    if (argc < 2) {
       fprintf(stderr, "Usage: %s <infile>\n", *argv);
@@ -38,10 +42,9 @@ int main(int argc, char **argv) {
    host_vector<float> elements(numElements);
    readElements(fp, numElements, &elements);
       
-   for (int i = 0; i < numElements; i++) {
-      cout << elements[i] << "\n";
-   }  
-   
+   device_vector<float> d_elements = elements;
+   mean = reduce(d_elements.begin(), d_elements.end(), 0.0, plus<float>()) / numElements;   
+   std::cout << mean << "\n";   
+
    return 0; 
-   
 }
